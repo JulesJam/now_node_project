1b105a0265ee0201883600c308bc37b3be6ee95c
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2017-11-20 17:21:23 +0000

    express set up to use static path to public folder

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require ('express')
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req,res)=>{
+  /*res.send(`<h1>Hello World</h1>
+    <h2>The Current Time is: ${new Date().toISOString()}!</h2>`)*/
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+})
